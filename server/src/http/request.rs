fdfb4968193b999317e6e4285c1d5b757e57b96f
Vinicius Komninakis <vinicius_k2013@hotmail.com>
2022-04-05 00:15:55 -0300

    feat: parse url from string buffer

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -17,8 +17,28 @@ impl TryFrom<&[u8]> for Request {
     // GET /search/?name=abc&sort=1 HTTP/1.1
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(value)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        // this re-use variable is called variable shadowing
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
 }
 
 pub enum ParseError {
